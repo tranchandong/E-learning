3adbc187de44fa2783651b593653f808023e60a8
dongtran <mar30hirai@gmail.com>
2023-05-10 22:00:46 +0700

    ok

---
+++ src/services/quanLyKhoaHoc.services.ts
@@ -0,0 +1,72 @@
+export type GetFetchCoursesResponse = {
+  maKhoaHoc: string;
+  biDanh: string;
+  tenKhoaHoc: string;
+  moTa: string;
+  luotXem: number;
+  hinhAnh: string;
+  maNhom: string;
+  ngayTao: string;
+  soLuongHocVien: number;
+  nguoiTao: {
+    taiKhoan: string;
+    hoTen: string;
+    maLoaiNguoiDung: string;
+    tenLoaiNguoiDung: string;
+  };
+  danhMucKhoaHoc: {
+    maDanhMucKhoahoc: string;
+    tenDanhMucKhoaHoc: string;
+  };
+};
+
+export type GetFetchCategoryResponse = {
+    maDanhMuc: string,
+    tenDanhMuc: string,
+};
+
+export type GetFetchCoursesByCategory = {
+    maKhoaHoc: string;
+    biDanh: string;
+    tenKhoaHoc: string;
+    moTa: string;
+    luotXem: number;
+    hinhAnh: string;
+    maNhom: string;
+    ngayTao: string;
+    soLuongHocVien: number;
+    nguoiTao: {
+      taiKhoan: string;
+      hoTen: string;
+      maLoaiNguoiDung: string;
+      tenLoaiNguoiDung: string;
+    };
+    danhMucKhoaHoc: {
+      maDanhMucKhoahoc: string;
+      tenDanhMucKhoaHoc: string;
+    };
+}
+
+export type GetFetchCoursesDetail = {
+  maKhoaHoc: string;
+  biDanh: string;
+  tenKhoaHoc: string;
+  moTa: string;
+  luotXem: number;
+  hinhAnh: string;
+  maNhom: string;
+  ngayTao: string;
+  soLuongHocVien: number;
+  nguoiTao: {
+    taiKhoan: string;
+    hoTen: string;
+    maLoaiNguoiDung: string;
+    tenLoaiNguoiDung: string;
+  };
+  danhMucKhoaHoc: {
+    maDanhMucKhoahoc: string;
+    tenDanhMucKhoaHoc: string;
+  };
+}
+
+export const quanLyKhoaHocServices = {};
